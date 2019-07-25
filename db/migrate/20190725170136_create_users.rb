bebf7c1f0c6d0a82c84dea008cfce2af438b2886
Tad Preston <tadpreston@gmail.com>
2019-07-25 18:21:31 -0500

    changed the users schema

--- db/migrate/20190725170136_create_users.rb
+++ db/migrate/20190725170136_create_users.rb
@@ -1,12 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :username
 
       t.timestamps
     end
 
-    add_index :users, :email
+    add_index :users, :username
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,12 @@ ActiveRecord::Schema.define(version: 2019_07_25_170136) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
