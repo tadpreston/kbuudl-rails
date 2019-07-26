0c171a0621179293e0e2dc815911809f65cda4f5
Tad Preston <tadpreston@gmail.com>
2019-07-26 12:06:56 -0500

    Testing validations and removing pending tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it do
+      should validate_presence_of(:first_name)
+      should validate_presence_of(:last_name)
+      should validate_presence_of(:username)
+      should validate_length_of(:username).is_at_least(1).is_at_most(100)
+    end
+  end
 end
