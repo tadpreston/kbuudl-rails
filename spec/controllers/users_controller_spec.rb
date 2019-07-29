1e37d9340e48f9ccb54a27e60e4296d8c32eda5b
Tad Preston <tadpreston@gmail.com>
2019-07-29 11:59:58 -0500

    Added tests for the users new and create methods

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
 
+  describe "POST #create" do
+    let(:user_params) { { :user => { first_name: "Bob", last_name: "Smith", username: "bobsmith" } } }
+
+    context "with valid attributes" do
+      it "inserts the user into the database" do
+        expect {
+          post :create, :params => user_params
+        }.to change(User, :count).by(1)
+      end
+      it "redirects to the home page" do
+        post :create, :params => user_params
+        expect(response).to redirect_to(root_url)
+      end
+    end
+    context "with invalid attributes" do
+      let(:user_params) { { :user => { first_name: "", last_name: "", username: "" } } }
+      it "renders the new template" do
+        post :create, :params => user_params
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "pages/index.html.erb", type: :view do
+  it "displays the home page" do
+    render
+
+    expect(response.body).to match(/Collect all of your stuff/)
+  end
 end
