a4600993600486e4ab300517e49b129480ad08a7
Tad Preston <tadpreston@gmail.com>
2019-07-30 22:07:21 -0500

    added the credentials association to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :credentials
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true, length: { in: 1..100 }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'validations' do
+  describe 'associations' do
+    it { should have_many(:credentials) }
+  end
+
+  describe 'validations' do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:username) }
