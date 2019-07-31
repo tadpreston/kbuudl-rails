41c6692e7098e0732fa0fd313b18bff54cfcd123
Tad Preston <tadpreston@gmail.com>
2019-07-30 22:01:31 -0500

    tests for the credential model

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -1,4 +1,41 @@
 class Credential < ApplicationRecord
+  attr_accessor :password, :password_confirmation
+
+  MAX_PASSWORD_LENGTH = 72
+
   belongs_to :user
-  has_secure_password
+
+  validates :email, presence: true
+  validates :password, length: { maximum: MAX_PASSWORD_LENGTH }
+  validates :password, confirmation: true
+  validate do |record|
+    record.errors.add(:password, :blank) unless record.password_digest.present?
+  end
+
+  def password=(unencrypted_password)
+    if unencrypted_password.blank?
+      self.password_digest = nil
+    elsif unencrypted_password
+      @password = unencrypted_password
+      self.password_digest = hasher.create(unencrypted_password)
+    end
+  end
+
+  def password_confirmation=(unencrypted_password)
+    @password_confirmation = unencrypted_password
+  end
+
+  def authenticate(unencrypted_password)
+    Argon2::Password.verify_password(unencrypted_password, password_digest) && self
+  end
+
+  private
+
+  def password_digest?
+    password_digest
+  end
+
+  def hasher
+    Argon2::Password.new(t_cost: 2, m_cost: 16)
+  end
 end
--- spec/models/credential_spec.rb
+++ spec/models/credential_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Credential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_length_of(:password).is_at_most(Credential::MAX_PASSWORD_LENGTH) }
+    it { should validate_presence_of(:password) }
+    it { should validate_confirmation_of(:password) }
+  end
+
+
+
+  describe ".password=" do
+    subject(:credential) { build(:credential, password: password) }
+
+    context "password is not blank" do
+      let(:password) { "supersekret" }
+      it "sets the password_digest" do
+        expect(credential.password_digest).to_not be_nil
+      end
+      it "encrypts the password" do
+        expect(credential.password_digest).to_not eq(password)
+      end
+    end
+    context "password is blank" do
+      let(:password) { "" }
+      it "sets the password_digest to nil" do
+        expect(credential.password_digest).to be_nil
+      end
+    end
+    context "password is nil" do
+      let(:password) { nil }
+      it "does nothing" do
+        expect(credential.password_digest).to be_nil
+      end
+    end
+  end
+
+  describe ".authenticate" do
+    let(:password) { "supersekret" }
+    subject(:credential) { create(:credential, password: password, password_confirmation: password) }
+
+    context "with a valid password" do
+      it "returns true" do
+        expect(credential.authenticate(password)).to eq(credential)
+      end
+    end
+    context "with an invalid password" do
+      let(:invalid_password) { "invalid" }
+      it "returns true" do
+        expect(credential.authenticate(invalid_password)).to eq(false)
+      end
+    end
+  end
 end
