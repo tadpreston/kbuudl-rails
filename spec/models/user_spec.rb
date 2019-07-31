c1ac0f21f78afd10fccaf1ff15a7abceae1aca98
Tad Preston <tadpreston@gmail.com>
2019-07-30 22:04:24 -0500

    refactored the user validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validations' do
-    it do
-      should validate_presence_of(:first_name)
-      should validate_presence_of(:last_name)
-      should validate_presence_of(:username)
-      should validate_length_of(:username).is_at_least(1).is_at_most(100)
-    end
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_length_of(:username).is_at_least(1).is_at_most(100) }
   end
 end
